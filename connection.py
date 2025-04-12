9324e25d6282444cfa00774db07a4d9dc9c1079f
Minh Thinh <22280087@student.hcmus.edu.vn>
2025-04-13 00:59:33 +0700

    test connection

--- connection.py
+++ connection.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-MONGODB_URI = "mongodb+srv://myAtlasDBUser:thinh4526@myatlasclusteredu.7ezulr9.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU"
+# replace <password> with your actual password
+MONGODB_URI = "mongodb+srv://myAtlasDBUser:<password>@myatlasclusteredu.7ezulr9.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU"
 
 client = MongoClient(MONGODB_URI)
 
